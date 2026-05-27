e06acb00faf640606cfe3f87c43e5b45a525682c
Jonathan Wong <jonwong84@gmail.com>
2026-05-27 09:51:33 -0400

    WIP EF migration ready

---
+++ src/Jukebox.DataAccess.Migrations/JukeboxDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Jukebox.DataAccess.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Jukebox.DataAccess.Migrations;
+
+public class JukeboxDbContextFactory : IDesignTimeDbContextFactory<JukeboxDbContext>
+{
+    public JukeboxDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<JukeboxDbContext>();
+
+        var connectionString = Environment.GetEnvironmentVariable("JUKEBOX_DB_CONNECTION_STRING")
+            ?? throw new InvalidOperationException("The environment variable 'JUKEBOX_DB_CONNECTION_STRING' is not set.");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new JukeboxDbContext(optionsBuilder.Options);
+    }
+}
